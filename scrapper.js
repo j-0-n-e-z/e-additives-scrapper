c4c3c3619df3b0b0e4eddf192ecb5faa9c854cca
j-0-n-e-z <j000neeez@gmail.com>
2023-08-29 17:03:21 +0500

    refactor

--- scrapper.js
+++ scrapper.js
@@ -16,25 +16,27 @@ async function getAdditives(url) {
 
 	await page
 		.waitForSelector('.pager__item', { timeout: 4000, hidden: true })
-		.catch((e) => console.log(e))
-
-	const addElements = await page.$$('.addicon')
+		.catch(e => console.log(e))
 
 	const originElements = await page.$$('.term--additive-origins')
 
 	const allOrigins = {}
 	for (const originElement of originElements) {
 		const number = await originElement.evaluate(el => el.classList[1].slice(-2))
-		const origin = await originElement.evaluate(el => el.innerText.toLowerCase())
+		const origin = await originElement.evaluate(el =>
+			el.innerText.toLowerCase()
+		)
 		allOrigins[number] = origin.trim()
 	}
 
 	console.log(allOrigins)
 
 	const adds = []
+	const addElements = await page.$$('.addicon')
 
 	for (const addElement of addElements) {
 		const danger = await addElement.evaluate(el => +el.classList[1].at(-1))
+
 		const originElements = await addElement.$$('.addicon__origin-item')
 		const origins = []
 		for (const originElement of originElements) {
@@ -43,17 +45,19 @@ async function getAdditives(url) {
 			)
 			origins.push(allOrigins[originNumber])
 		}
+
 		const [code, name] = await addElement.$eval('.addicon__link', node =>
 			node.innerText.split(' – ')
 		)
 
 		adds.push({ id: code, code, name: name.toLowerCase(), danger, origins })
 	}
-	
+
 	console.log(adds)
+
 	if (adds.length) {
-		fs.writeFileSync('additives.json', JSON.stringify(adds), err =>
-			console.log(err)
+		fs.writeFileSync('additives.json', JSON.stringify(adds), e =>
+			console.log(e)
 		)
 	}
 
